42fc7e19b3690a3c0b63548e8978b153fc6b36be
Nicholas <nicholasdipasquo@libero.it>
2021-02-13 18:26:41 +0100

    Add verifier

--- src/main/java/it/unimol/Aeroporti/Ui/InterfaceException.java
+++ src/main/java/it/unimol/Aeroporti/Ui/InterfaceException.java
@@ -5,7 +5,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 
-public class InterfaceException { //interfaccia eccezione riguardante il caso del passaporto e nel caso venga acuistato lo stesso biglietto 2 volte
+public class InterfaceException {//interfaccia eccezione riguardante il caso del passaporto e nel caso venga acuistato lo stesso biglietto 2 volte
     private JPanel panelInterfaceException;
     private JButton exitButton;
     public ChangeInterface changeInterface;
